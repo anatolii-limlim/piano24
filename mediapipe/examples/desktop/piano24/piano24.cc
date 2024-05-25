bf6b0685529bde4faa0e0cbf45a7500c1958ac5e
anatolii.limlim <anatolii.limlim@gmail.com>
2024-05-25 22:40:15 +0400

    remove HW

--- mediapipe/examples/desktop/piano24/piano24.cc
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-void piano_24_main() {
-    std::cout << "Hello World!\n";
-    std::cout << "Hello World!\n";
-}
--- mediapipe/examples/desktop/piano_24_run.cc
+++
@@ -1,5 +0,0 @@
-#include "piano24/piano24.h";
-
-int main() {
-    piano_24_main();
-}
